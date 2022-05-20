5cc4fabab6237e497147a7ed8c32f377ff8cc24a
Piotr <pepapek@gmail.com>
2022-05-20 12:20:34 +0200

    Logo in src folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Game from "./Components/Game";
+import logo from "./img/logo.png";
 import React, { useState } from "react";
 function App() {
 	const [isStarted, setIsStarted] = useState(false);
@@ -12,7 +13,7 @@ function App() {
 	}
 	return (
 		<section>
-			<img className="logo" src="img/logo.png" alt="" />
+			<img className="logo" src={logo} alt="" />
 			{isStarted ? (
 				<Game drinkAmount={5} variantAmount={3} onQuit={handleQuit}></Game>
 			) : (
